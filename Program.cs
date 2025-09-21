b6e8d029924a3bf39822d164e4a13af3eb15bf45
Marachinos <stallnysater@gmail.com>
2025-09-21 11:25:04 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace bankaccount
+{
+    class Program
+    {
+        class BankAccount //class representing a bank account,
+                          //with methods to deposit and withdraw money
+        {
+            private decimal balance;
+            public decimal Balance => balance;
+
+            public void Deposit(decimal amount)
+            {
+                if (amount > 0) balance += amount;
+            }
+
+            public void Withdraw(decimal amount)
+            {
+                if (amount > 0 && amount <= balance)
+                    balance -= amount;
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
